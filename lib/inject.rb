0823dc987aab71fadb30983b0b602b9d24c18dc5
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-12-06 14:24:27 +0000

    Refactored and improved tests and code for Inject

--- lib/inject.rb
+++ lib/inject.rb
@@ -2,13 +2,13 @@ class Array
 
 def jectin(args, &block)
 	sum = args
-	self.each do|x|
-		sum += yield(x)
+	self.each do |x|
+		sum = yield(sum, x)
 	end
 	sum
 end
 end
 
- # p [2,3,4].jectin() {|x| x * 2}
+  # p [2,3,4].jectin(10) {|x,y| x * 2}
 
 
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -5,18 +5,22 @@ describe Array do
 let(:array) {[2,3,4]}
 
 it 'should be able to multiply the values of an array by 2' do
-	expect(array.jectin(1) {|x| x * 2}).to eq(19)
+	expect(array.jectin(2) {|x,y| x + y * 2}).to eq(20)
 end
 
 it 'should contain an argument to define JECTIN' do
-		expect{array.jectin() {|x| x * 2}}.to raise_error
+		expect{array.jectin() {|x,y| x * 2}}.to raise_error
 	end
 
 it 'should be able to do sum of numbers ' do
-	expect(array.jectin(0) {|x| x + 2}).to eq(15)
+	expect(array.jectin(0) {|x,y| x + y + 2}).to eq(15)
+
 end
 
+it 'should be able to inject arrays ' do
+	expect(array.jectin([]) {|x,y| x << y**2 }).to eq([4,9,16])
 
+end
 
 	
 end
